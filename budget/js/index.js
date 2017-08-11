32d9f80af9c109ce2a50b415a425f94f522a6f53
Michael Girgis <mgirgis@duoconsulting.com>
2017-08-11 10:06:53 -0500

    Updates

--- budget/js/index.js
+++ budget/js/index.js
@@ -1,34 +1,36 @@
+$('#loading').css('opacity', 1);
+
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     window.location = 'expenses.html';
   } else {
-    // Do nothing
+    // Hide the loading message, and enable the login button.
+    $('#loading').hide();
+    $('#login').show();
   }
 });
 
-// firebase.auth().getRedirectResult().then(function(result) {
-//   console.log('Result from getRedirectResult:');
-//   console.log(result)
+firebase.auth().getRedirectResult().then(function(result) {
+  console.log('redirect');
 
-//   if (result.credential) {
-//     // This gives you a Google Access Token. You can use it to access the Google API.
-//     var token = result.credential.accessToken;
-//     // ...
-//   }
-//   // The signed-in user info.
-//   var user = result.user;
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  var user = result.user;
 
-//   $('body').append('<p>Got redirect result!</p>');
-// }).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // The email of the user's account used.
-//   var email = error.email;
-//   // The firebase.auth.AuthCredential type that was used.
-//   var credential = error.credential;
-//   // ...
-// });
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
 
 $('#login').on('click', function(e) {
   e.preventDefault();
